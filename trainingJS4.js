28057a4b3a0b137758a7ea20baa3342b7c1d7646
Haadi Shakir <hmshakir2@gmail.com>
2025-02-18 15:54:19 -0600

    easy-ish

---
+++ trainingJS4.js
@@ -0,0 +1,27 @@
+function getLength(arr){
+    //return length of arr
+    let p = arr.length
+    return p
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    let r = arr[0]
+    return r
+  }
+  function getLast(arr){
+    //return the last element of arr
+    let j = arr[arr.length - 1]
+    return j
+  }
+  function pushElement(arr){
+    var el=1;
+    let y = arr.push(el)
+    //push el to arr
+    
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    let o = arr.pop()
+    return arr
+  }
