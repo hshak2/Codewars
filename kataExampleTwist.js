2c1d09c9214cb21a2a7a2217abd0289e2bad1960
Haadi Shakir <hmshakir2@gmail.com>
2025-02-18 19:17:48 -0600

    easy

---
+++ kataExampleTwist.js
@@ -0,0 +1,7 @@
+// add the value "codewars" to the websites array 1,000 times
+let websites = []
+const count = 1000
+const val = 'codewars'
+for(let i = 0; i < count; i++){
+  websites.push(val);
+}
