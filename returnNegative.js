7f368d3909f6bdae39243474689a5113fdce987e
Haadi Shakir <hmshakir2@gmail.com>
2025-02-19 08:56:26 -0600

    easy-ish question

---
+++ returnNegative.js
@@ -0,0 +1,12 @@
+function makeNegative(num) {
+    // Code?
+    if(num > 0){
+      return num = num * -1
+    }else if(num < 0){
+      return num = num * 1
+    }else{
+      return num
+    }
+  }
+
+  
