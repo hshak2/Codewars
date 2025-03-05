7475b2b27c7d3b740176e565e558809ee93d4ab3
Haadi Shakir <hmshakir2@gmail.com>
2025-03-05 13:48:40 -0600

    8kyu problem that required knowledge of SA of a box formula and volume of a box

---
+++ SAAndVolOfABox.js
@@ -0,0 +1,11 @@
+function getSize(width, height, depth){
+    let SA
+    if(width === height && width === depth){
+      SA = 6 * width * width
+    }else{
+      SA = 2 * depth * width + 2 * depth * height + 2 * width * height
+    }
+      let vol = width * height * depth
+      return [SA, vol]
+  }
+  
