740fee744b15733386d79ecd3871db96a825c5d7
Haadi Shakir <hmshakir2@gmail.com>
2025-03-08 14:49:36 -0600

    8kyu problem

---
+++ plural.js
@@ -0,0 +1,12 @@
+function plural(n) {
+    // ...
+  if(n === 0 || n > 0 && n < 1){
+    return true
+  }else if(n === 1){
+    return false
+  }else if(n > 1){
+    return true
+  }else{
+    return true
+  }
+  }
