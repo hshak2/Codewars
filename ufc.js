a8ae088c6a393c1031c04eb0f787472cff601c5b
Haadi Shakir <hmshakir2@gmail.com>
2025-04-15 21:52:47 -0500

    7kyu problem

---
+++ ufc.js
@@ -0,0 +1,8 @@
+function quote(fighter) {
+    // your code here
+    if(fighter.toLowerCase() === 'george saint pierre'){
+      return 'I am not impressed by your performance.'
+    }else if(fighter.toLowerCase() === 'conor mcgregor'){
+      return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    }
+  };
