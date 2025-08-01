32e1bce0f60e4526043e390004d745aa6a1f7b85
Haadi Shakir <hmshakir2@gmail.com>
2025-07-31 23:55:42 -0500

    additional comment

--- calculateTheAreaOfNSides.js
+++ calculateTheAreaOfNSides.js
@@ -3,3 +3,4 @@ function areaOfPolygonInsideCircle(r, s) {
   let area = r * r * s * Math.sin(angle * 2) / 2
   return +area.toFixed(3)
 }
+//added this extra line just for the green square because it has been a difficult day so will do an extra kata tomorrow
