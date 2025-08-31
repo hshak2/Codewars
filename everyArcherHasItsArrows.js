184567afbf4fc67ae743c3eb1d41e2cf80b117e9
Haadi Shakir <hmshakir2@gmail.com>
2025-08-31 13:38:26 -0500

    7kyu every array method

---
+++ everyArcherHasItsArrows.js
@@ -0,0 +1,10 @@
+function archersReady(archers){
+if(archers.every(e => e >= 5) === true && archers.length > 0){
+  return true
+}else if(archers.length === 0){
+  return false
+}else{
+  return false
+}
+
+}
