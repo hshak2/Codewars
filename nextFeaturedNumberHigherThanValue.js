aa9141a03641d7db9277314f51aa3b2667aab80e
Haadi Shakir <hmshakir2@gmail.com>
2025-12-04 21:26:50 -0600

    7kyu uses Set object

---
+++ nextFeaturedNumberHigherThanValue.js
@@ -0,0 +1,8 @@
+function nextNumb(val) {
+  for (let i = val + 1; i < 9999999999; i++) {
+    if (new Set([...String(i)]).size == [...String(i)].length && i % 2 && i % 3 == 0) {
+      return i
+    }
+  }
+  return 'There is no possible number that fulfills those requirements'
+}
