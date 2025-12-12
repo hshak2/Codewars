b9c969f7a5376135bc5e655908fa4d70cbf5995a
Haadi Shakir <hmshakir2@gmail.com>
2025-12-11 23:36:33 -0600

    redo of a problem

--- thePowerOfExponents.js
+++ thePowerOfExponents.js
@@ -1,3 +1,4 @@
 function power(base, exponent){
   return base** exponent
 }
+//adding an additional line of code here because I had a busy day today and yesterday but will make up for the codewars tomorrow hopefully
